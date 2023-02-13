053c3ea7e31c0bd8146d894bd476a214c099782a
iqionly <rizky@kabayan.id>
2023-02-13 13:02:44 +0000

    Schedule every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('synclog')->everyTwoMinutes();
+        $schedule->command('command:synclog')->everyMinute();
     }
 
     /**
