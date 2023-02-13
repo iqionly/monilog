fd38a8445373a1e5aca16a72d41ac1de1d177730
iqionly <izzy25.mr@gmail.com>
2023-02-13 20:08:36 +0700

    fixing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('synclog')->everyMinute();
+        $schedule->command('command:synclog')->everyMinute();
     }
 
     /**
